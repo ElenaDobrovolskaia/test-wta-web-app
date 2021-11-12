9a827fe07f700dc0b754e24d8efcde32c8f6990a
Elena <64628545+ElenaDobrovolskaia@users.noreply.github.com>
2021-11-11 17:42:18 -0800

    Update MyHikesAndRecommendations.java

--- pages/MyHikesAndRecommendations.java
+++ pages/MyHikesAndRecommendations.java
@@ -11,17 +11,18 @@ import static support.TestContext.getActions;
 import static support.TestContext.getDriver;
 
 public class MyHikesAndRecommendations extends Header {
-    @FindBy(xpath = "//div[@class='module view list highlight backpack-hikes']")
+   @FindBy(css = "div[class$='backpack-hikes']")
     private WebElement savedHikes;
     private WebElement savedHikeCard(String title) {
         return getDriver().findElement(By.xpath("//a[contains(text(),'"+title+"')]"));
     }
     private WebElement removeHikeButton(String title){
-        return getDriver().findElement(By.xpath("//a[contains(text(),'"+title+"')]/../..//div/span[contains(text(),'Remove Hike')]"));
+        //return getDriver().findElement(By.xpath("//a[contains(text(),'"+title+"')]/../..//div/span[contains(text(),'Remove Hike')]"));
+        return getDriver().findElement(By.cssSelector("div[data-hikename*='"+title+"']>div>div>div>span[class*='remove']"));
     }
 
     public List<WebElement> getSavedHikesList(){
-        List<WebElement>savedHikes=getDriver().findElements(By.xpath("//div[@class='module view list highlight backpack-hikes']"));
+        List<WebElement>savedHikes=getDriver().findElements(By.cssSelector("div[class*='backpack']>div:nth-of-type(n)"));
         return savedHikes;
     }
     public MyHikesAndRecommendations removeHikeFromList(String hikeName){
