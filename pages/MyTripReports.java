45c53e30afb43d1a0f487a051b5de0860ea67759
Elena <64628545+ElenaDobrovolskaia@users.noreply.github.com>
2021-11-13 20:22:45 -0800

    Update MyTripReports.java

--- pages/MyTripReports.java
+++ pages/MyTripReports.java
@@ -9,11 +9,10 @@ import java.util.List;
 import static support.TestContext.getDriver;
 
 public class MyTripReports extends Header{
-    @FindBy (id = "trip-reports")
-    private WebElement allTripReports;
-
+   
     public List<WebElement> getMyTripReportsList(){
         List<WebElement> myTripReports=getDriver().findElements(By.cssSelector("#trip-reports>div>div:nth-of-type(n)"));
         return myTripReports;
     }
+    
 }
